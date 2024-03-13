b125531b9465c00d7b9ea53f5773662979e67afe
Jhon Cruz <jhonskaz@gmail.com>
2024-03-13 17:53:23 -0300

    Adiciona tabela de transações

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,9 +1,48 @@
 import { Header } from "../../components/Header/Header";
+import { Summary } from "../../components/Summary/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header></Header>
+      <Summary></Summary>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 10.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/03/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hospedagem de site</td>
+              <td>
+                <PriceHighlight variant="outcome">R$ -2.000,00</PriceHighlight>
+              </td>
+              <td>Compra</td>
+              <td>13/03/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                {" "}
+                <PriceHighlight variant="income">R$ 2.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/03/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
