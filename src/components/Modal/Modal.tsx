8d531f672560398a656ed4d34e7465bfbf8a3185
Jhon Cruz <jhonskaz@gmail.com>
2024-03-14 14:47:21 -0300

    Adiciona estilos no modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,24 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from "@phosphor-icons/react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+      <Content>
+        <Dialog.Title>Nova transação</Dialog.Title>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+        <form>
+          <input type="text" placeholder="Descrição" required />
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Dialog.Description />
+      </Content>
+    </Dialog.Portal>
+  );
+}
